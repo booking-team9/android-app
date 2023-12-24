7e6923227ad23ccf34dbdc9c776b757c9a7743ce
aleksaqm <perovic.aleksa03@gmail.com>
2023-12-24 15:52:26 +0100

    feat: created host-accommodations fragment

--- app/src/main/java/com/example/bookingappteam9/activities/MainActivity.java
+++ app/src/main/java/com/example/bookingappteam9/activities/MainActivity.java
@@ -2,16 +2,15 @@ package com.example.bookingappteam9.activities;
 
 import android.os.Bundle;
 
-import com.example.bookingappteam9.R;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import com.example.bookingappteam9.R;
 import com.example.bookingappteam9.databinding.ActivityMainBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_profile, R.id.navigation_properties)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
