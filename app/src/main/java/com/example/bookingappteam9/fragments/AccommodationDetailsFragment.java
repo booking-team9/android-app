9719254991bfb6c62be1fd46492d81c12b2d9eac
danilo_c <danilo.cvijetic10@protonmail.ch>
2024-01-10 21:41:51 +0100

    feat: accommodation details page

--- app/src/main/java/com/example/bookingappteam9/fragments/AccommodationDetailsFragment.java
+++ app/src/main/java/com/example/bookingappteam9/fragments/AccommodationDetailsFragment.java
@@ -20,10 +20,18 @@ import android.view.ViewGroup;
 import com.example.bookingappteam9.R;
 import com.example.bookingappteam9.adapters.AmenityAdapter;
 import com.example.bookingappteam9.adapters.DetailsPhotosAdapter;
+import com.example.bookingappteam9.adapters.ReviewsAdapter;
 import com.example.bookingappteam9.clients.ClientUtils;
 import com.example.bookingappteam9.databinding.FragmentAccommodationDetailsBinding;
 import com.example.bookingappteam9.model.Accommodation;
 import com.example.bookingappteam9.model.Photo;
+import com.example.bookingappteam9.model.Review;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.material.carousel.CarouselLayoutManager;
 import com.google.android.material.carousel.CarouselSnapHelper;
 import com.google.android.material.carousel.HeroCarouselStrategy;
@@ -41,9 +49,11 @@ public class AccommodationDetailsFragment extends Fragment {
     private FragmentAccommodationDetailsBinding binding;
     private DetailsPhotosAdapter photoAdapter;
     private AmenityAdapter amenityAdapter;
+    private ReviewsAdapter reviewsAdapter;
     private  Long accommodationId;
     private Boolean isFavorite = false;
     private Accommodation accommodation;
+    private SupportMapFragment mapFragment;
 
     public static AccommodationDetailsFragment newInstance() {
         return new AccommodationDetailsFragment();
@@ -64,6 +74,9 @@ public class AccommodationDetailsFragment extends Fragment {
         List<String> photos = new ArrayList<>();
         photoAdapter = new DetailsPhotosAdapter(photos);
         amenityAdapter = new AmenityAdapter(new ArrayList<>());
+        reviewsAdapter = new ReviewsAdapter(new ArrayList<>());
+        binding.accommodationDetailsReviews.setAdapter(reviewsAdapter);
+        binding.accommodationDetailsReviews.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false));
         binding.carouselRecyclerView.setAdapter(photoAdapter);
         binding.carouselRecyclerView.setLayoutManager(new CarouselLayoutManager(new HeroCarouselStrategy()));
         binding.accommodationDetailsAmenities.setAdapter(amenityAdapter);
@@ -114,16 +127,50 @@ public class AccommodationDetailsFragment extends Fragment {
         binding.accommodationDetailsAddress.setText(accommodation.getAddress().toString());
         binding.accommodationDetailsType.setText(accommodation.getAccommodationType());
         binding.accommodationDetailsDescription.setText(accommodation.getDescription());
+        binding.accommodationDetailsCancellationDeadline.setText(accommodation.getCancellationDeadline().toString() + " days");
         StringBuilder host = new StringBuilder();
         host.append(accommodation.getHost().getFirstName()).append(" ").append(accommodation.getHost().getLastName());
         binding.accommodationDetailsHost.setText(host);
+        ClientUtils.reviewService.getByAccommodationId(accommodationId).enqueue(new Callback<ArrayList<Review>>() {
+            @Override
+            public void onResponse(Call<ArrayList<Review>> call, Response<ArrayList<Review>> response) {
+                if (response.isSuccessful()){
+                    reviewsAdapter.addAll(response.body());
+                    if (mapFragment != null) {
+                        mapFragment.getMapAsync(callback);
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ArrayList<Review>> call, Throwable t) {
+                Log.d("QM", t.getMessage() != null ? t.getMessage() : "error");
+
+            }
+        });
+
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         mViewModel = new ViewModelProvider(this).get(AccommodationDetailsViewModel.class);
+        mapFragment =
+                (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.accommodation_details_map);
     }
+
+
+    private OnMapReadyCallback callback = new OnMapReadyCallback() {
+        @Override
+        public void onMapReady(GoogleMap googleMap) {
+            googleMap.getUiSettings().setZoomControlsEnabled(true);
+            googleMap.getUiSettings().setZoomGesturesEnabled(false);
+            LatLng apartment = new LatLng(accommodation.getAddress().getLatitude(), accommodation.getAddress().getLongitude());
+            googleMap.addMarker(new MarkerOptions().position(apartment).title(accommodation.getName()));
+            googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(apartment, 12));
+
+        }
+    };
 
 
 }
