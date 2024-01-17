2de25d2e093d792c4f611dd0d3ebd015696de439
aleksaqm <perovic.aleksa03@gmail.com>
2024-01-17 18:16:01 +0100

    feat: added viewModel for edit accommodation

--- app/src/main/java/com/example/bookingappteam9/services/AccommodationService.java
+++ app/src/main/java/com/example/bookingappteam9/services/AccommodationService.java
@@ -14,6 +14,7 @@ import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.PATCH;
 import retrofit2.http.POST;
+import retrofit2.http.PUT;
 import retrofit2.http.Path;
 
 public interface AccommodationService {
@@ -41,6 +42,8 @@ public interface AccommodationService {
     Call<NewAccommodation> createAccommodation(@Body NewAccommodation accommodation);
     @POST("accommodations/get-availability-price")
     Call<PriceResponse> getPrice(@Body PriceRequest priceRequest);
+    @PUT("accommodations/update")
+    Call<NewAccommodation> updateAccommodation(@Body NewAccommodation accommodation);
 
 
 }
